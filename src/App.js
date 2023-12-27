66810ac15636b34c7a020d7f63d5bffdddf67abd
Omar Al <omaral121510@gmail.com>
2023-12-26 19:36:29 -0500

    initial project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home";
+import Legal from "./Pages/Legal";
+import NotFound from "./Pages/NotFound";
+import Appointment from "./Pages/Appointment";
+
+function App() {
+  return (
+    <div className="App">
+      <Router basename="/Health-Plus">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/legal" element={<Legal />} />
+          <Route path="/appointment" element={<Appointment />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Legal.js
@@ -0,0 +1,8 @@
+import React from "react";
+import LegalDocs from "../Components/LegalDocs";
+
+function Legal() {
+  return <LegalDocs />;
+}
+
+export default Legal;
